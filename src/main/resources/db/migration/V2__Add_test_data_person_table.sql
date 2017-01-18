deeac07fdbfd987810912dcde5b5e90657b90d6c
YingjiaZhang <zhyingjia@163.com>
2017-01-18 17:52:58 +0800

    add flyway demo

---
+++ src/main/resources/db/migration/V2__Add_test_data_person_table.sql
@@ -0,0 +1,4 @@
+insert into PERSON values(null,"sunwukong");
+insert into PERSON values(null,"tangseng");
+insert into PERSON values(null,"shaheshang");
+insert into PERSON values(null,"bailoongma");
